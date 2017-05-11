1a6b4115160c2191c7bc7b8308f699732be0fd0c
Lyuba Eremeeva <leremeeva174@gmail.com>
2017-05-12 00:40:38 +0300

    for friday 12.05

---
+++ src/main/java/by/netcracker/bsuir/pz3/courses/Main.java
@@ -0,0 +1,22 @@
+package by.netcracker.bsuir.pz3.courses;
+
+import by.netcracker.bsuir.pz3.courses.dao.entity.UserDao;
+import by.netcracker.bsuir.pz3.courses.dao.entity.implementation.UserDaoImpl;
+import by.netcracker.bsuir.pz3.courses.entity.Course;
+import by.netcracker.bsuir.pz3.courses.entity.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+//        ApplicationContext applicationContext =
+//                new ClassPathXmlApplicationContext("/src/webapp/WEB-INF/applicationContext.xml");
+//        // troubles with path
+//
+//        Course course = (Course) applicationContext.getBean("course");
+//        System.out.println(course);
+////        UserDao userDao = UserDaoImpl.INSTANCE;
+////        userDao.addUser(new User("login", "password", "name",
+////                "lname", "mname"));
+    }
+}
